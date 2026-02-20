f913277de0e73804d0019894876d4ba8bff1174f
pa5096 <pa5096.srmist.edu.in>
2026-02-20 23:28:07 +0530

    Coded to print OOPS with String Join

--- OOPSBannerApp1.java
+++ OOPSBannerApp1.java
@@ -1,4 +1,13 @@
 public class OOPSBannerApp1 {
-	public static void main(String[] args) {
-		System.out.println("OOPS"); }
-		}
+
+    public static void main(String[] args) {
+
+        System.out.println(String.join(" "," ***** ", " ***** ", "****** ", " ***** "));
+        System.out.println(String.join(" ","*     *", "*     *", "*     *", "*     *"));
+        System.out.println(String.join(" ","*     *", "*     *", "*     *", "*      "));
+        System.out.println(String.join(" ","*     *", "*     *", "****** ", " ***** "));
+        System.out.println(String.join(" ","*     *", "*     *", "*      ", "      *")); 
+        System.out.println(String.join(" ","*     *", "*     *", "*      ", "*     *"));    
+        System.out.println(String.join(" "," ***** ", " ***** ", "*      ", " ***** "));
+    }
+}
