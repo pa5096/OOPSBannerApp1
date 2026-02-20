93ce0f2331ac448fbb072581d3534880d060eacc
pa5096 <pa5096.srmist.edu.in>
2026-02-20 23:09:26 +0530

    Coded to print OOPS Banner

--- OOPSBannerApp1.java
+++ OOPSBannerApp1.java
@@ -1,4 +1,16 @@
 public class OOPSBannerApp1 {
-	public static void main(String[] args) {
-		System.out.println("OOPS"); }
-		}
+
+    public static void main(String[] args) {
+
+        System.out.println("   ***        ***      ******        ***** ");
+        System.out.println(" **   **    **   **    **    **     **     ");
+        System.out.println("**     **  **     **   **     **   **     ");
+        System.out.println("**     **  **     **   **    **     **  ");
+        System.out.println("**     **  **     **   ******        ***");
+        System.out.println("**     **  **     **   **              **  ");
+        System.out.println("**     **  **     **   **               **     ");
+        System.out.println(" **   **    **    **   **              **       ");
+        System.out.println("   ***        ***      **         *****    ");
+
+    }
+}
