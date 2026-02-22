2642608cf5fc23bbd1428a44d1314f4058236982
pa5096 <pa5096.srmist.edu.in>
2026-02-22 17:57:42 +0530

    Coded to print OOPS Banner using Array Init

--- OOPSBannerApp1.java
+++ OOPSBannerApp1.java
@@ -1,4 +1,18 @@
-public class OOPSBannerApp1 {
-	public static void main(String[] args) {
-		System.out.println("OOPS"); }
-		}
+class OOPSBannerApp1 {
+    public static void main(String[] args) {
+        String[] banner = {
+			String.join(" ","   ***        ***      ******        ***** "),
+            String.join(" "," **   **    **   **    **    **     **     "),
+            String.join(" ","**     **  **     **   **     **   **     "),
+            String.join(" ","**     **  **     **   **    **     **  "),
+            String.join(" ","**     **  **     **   ******        ***"),
+            String.join(" ","**     **  **     **   **              **  "),
+            String.join(" ","**     **  **     **   **               **     "),
+            String.join(" "," **   **    **    **   **              **       "),
+            String.join(" ","   ***        ***      **         *****   ") 
+        };
+        for (String line : banner) {
+            System.out.println(line);
+        }
+    }
+}        
